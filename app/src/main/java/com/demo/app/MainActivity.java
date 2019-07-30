09e14364c92867ddcd491a8b70136baa028943dc
xugang <xugang@zto.cn>
2019-07-30 17:48:21 +0800

    验证码框

--- app/src/main/java/com/demo/app/MainActivity.java
+++ app/src/main/java/com/demo/app/MainActivity.java
@@ -1,13 +1,30 @@
 package com.demo.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                //验证码
+                startActivity(new Intent(this, VerificationCodeActivity.class));
+                break;
+            case R.id.button2:
+                //悬浮框
+                break;
+            default:
+                break;
+        }
     }
 }
