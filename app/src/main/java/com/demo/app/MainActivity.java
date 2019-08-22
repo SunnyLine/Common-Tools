87c648bb13d93804a8576481f7f8d0203b57095e
xugang <xugang@zto.cn>
2019-08-22 11:21:16 +0800

    权限判断

--- app/src/main/java/com/demo/app/MainActivity.java
+++ app/src/main/java/com/demo/app/MainActivity.java
@@ -1,17 +1,19 @@
 package com.demo.app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.pullein.common.android.view.CommonPopupWindow;
 import com.pullein.common.utils.DensityUtil;
-import com.pullein.common.utils.DesUtil;
 import com.zyyoona7.wheel.WheelView;
 
 import java.util.Arrays;
@@ -40,10 +42,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.button2:
                 //悬浮框
-                startService(new Intent(this,FlowService.class));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Settings.canDrawOverlays(this)) {
+                        startService(new Intent(this, FlowService.class));
+                    } else {
+                        new AlertDialog.Builder(this)
+                                .setMessage("需要取得权限以使用悬浮窗")
+                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                                        startActivityForResult(intent, 100);
+                                    }
+                                }).show();
+                    }
+                    return;
+                }
+                startService(new Intent(this, FlowService.class));
                 break;
             case R.id.button3:
-                popupWindow = new CommonPopupWindow(this, R.layout.layout_date,MATCH, DensityUtil.dip2px(this,340));
+                popupWindow = new CommonPopupWindow(this, R.layout.layout_date, MATCH, DensityUtil.dip2px(this, 340));
                 WheelView<String> wvDate = popupWindow.getViewManager().findView(R.id.wv_date);
                 WheelView<String> wvTime = popupWindow.getViewManager().findView(R.id.wv_time);
                 wvDate.setData(Arrays.asList("今天", "明天"));
@@ -52,6 +70,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 ) {
+            Toast.makeText(this, "----" + resultCode, Toast.LENGTH_SHORT).show();
         }
     }
 }
