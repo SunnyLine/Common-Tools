f215c5592cf77267fb146f14380c2950d5eb0f3b
xugang <xugang@zto.cn>
2019-09-06 15:44:30 +0800

    添加public 修饰符，改变访问权限

--- android_common_tool/src/main/java/com/pullein/common/android/CrashHandler.java
+++ android_common_tool/src/main/java/com/pullein/common/android/CrashHandler.java
@@ -127,7 +127,7 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         }
     }
 
-    class Builder {
+    public class Builder {
         String crashFileSavePath;
         boolean isPrintErrorLog = true;
         AppCrashListener appCrashListener;
