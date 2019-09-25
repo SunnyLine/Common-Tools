98e4d68e396e8aa756bf1d5b363d3acf4b0f2795
xugang <xugang@zto.cn>
2019-09-25 15:34:23 +0800

    微调Demo

--- app/src/main/java/com/demo/app/activity/StatusBarActivity.java
+++ app/src/main/java/com/demo/app/activity/StatusBarActivity.java
@@ -2,6 +2,7 @@ package com.demo.app.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.demo.app.R;
 
@@ -11,5 +12,18 @@ public class StatusBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_bar);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_white:
+                break;
+            case R.id.btn_blue:
+                break;
+            case R.id.btn_pic:
+                break;
+            default:
+                break;
+        }
     }
 }
