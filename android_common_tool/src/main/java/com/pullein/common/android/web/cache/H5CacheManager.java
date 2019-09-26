cf08405e8c423a7f969398f0befd6629c8b48a25
xugang <xugang@zto.cn>
2019-09-26 17:32:00 +0800

    微调缓存

--- android_common_tool/src/main/java/com/pullein/common/android/web/cache/H5CacheManager.java
+++ android_common_tool/src/main/java/com/pullein/common/android/web/cache/H5CacheManager.java
@@ -1,5 +1,11 @@
 package com.pullein.common.android.web.cache;
 
+import android.content.Context;
+import android.net.Uri;
+import android.webkit.WebResourceResponse;
+
+import com.pullein.common.utils.Log;
+
 public class H5CacheManager {
     private volatile static H5CacheManager mH5CacheManager = new H5CacheManager();
 
@@ -16,4 +22,33 @@ public class H5CacheManager {
         mCacheSetting = cacheSetting;
     }
 
+    public WebResourceResponse shouldInterceptRequest(final Context context, String requestUrl){
+        Uri requestUri = Uri.parse(requestUrl);
+        if (!isInterceptor(requestUri)){
+            return null;
+        }
+        String requestHost = requestUri.getHost();
+        String requestPath = requestUri.getPath();
+        String requestVersion = requestUri.getQueryParameter(mCacheSetting.getVersionKey());
+        return null;
+    }
+
+    private boolean isInterceptor(Uri requestUri) {
+        if (!mCacheSetting.isCacheEnable()) {
+            //缓存不可用
+            Log.d("缓存未开启");
+            return false;
+        }
+        if (requestUri.getHost() == null || !mCacheSetting.getCacheHostWhiteList().contains(requestUri.getHost())) {
+            Log.d("外部链接不做拦截 cur host = " + requestUri.getHost());
+            return false;
+        }
+        String path = requestUri.getPath();
+        if (path == null || !path.contains(".")) {
+            return false;
+        }
+        String suffixName = path.substring(path.lastIndexOf("."));
+        Log.d("suffixName = " + suffixName);
+        return mCacheSetting.getInterceptMap().containsKey(suffixName);
+    }
 }
